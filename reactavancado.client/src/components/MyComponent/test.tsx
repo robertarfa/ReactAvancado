a93c8c9296f90949a31fe41e94553de3ff94479d
Roberta Aguilar <roberta_rfa@hotmail.com>
2025-01-09 20:24:20 -0300

    newComp

---
+++ reactavancado.client/src/components/MyComponent/test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react'
+
+import MyComponent from '.'
+
+describe('<MyComponent />', () => {
+  it('should render the heading', () => {
+    const { container } = render(<MyComponent />)
+
+    expect(screen.getByRole('heading', { name: /MyComponent/i })).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
