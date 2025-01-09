9bf939ef1fc117f69f8fec4950ac7b56d3d5164c
Roberta Aguilar <roberta_rfa@hotmail.com>
2025-01-09 19:39:31 -0300

    fix-lint

--- reactavancado.client/.lintstagedrc.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  '*.{js,jsx,ts,tsx}': (filenames) => [
-    `prettier --write ${filenames.join(' ')}`,
-    `eslint --fix ${filenames.join(' ')}`,
-    `jest --bail --findRelatedTests ${filenames.join(' ')}`
-  ]
-}
